f6b5eae35535e47e854d20ead07a71de72a7098b
Imad Jerra <smartshader@gmail.com>
2020-10-24 09:25:28 -0400

    Adding Item relationship to Genre and Artist

--- Catalog/src/Catalog.Domain/Entities/Artist.cs
+++ Catalog/src/Catalog.Domain/Entities/Artist.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace Catalog.Domain.Entities
 {
-    public class Artist {}
+    public class Artist
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
 }
--- Catalog/src/Catalog.Domain/Entities/Genre.cs
+++ Catalog/src/Catalog.Domain/Entities/Genre.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace Catalog.Domain.Entities
 {
-    public class Genre {}
+    public class Genre
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public ICollection<Item> items { get; set; }
+    }
 }
