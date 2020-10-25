fa854450587705c41f8a17b9df2e68bd1c1ca61d
Imad Jerra <smartshader@gmail.com>
2020-10-25 00:08:37 -0400

    Add PostgreSQL configuration to startup.cs

--- Catalog/src/Catalog.API/Startup.cs
+++ Catalog/src/Catalog.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Catalog.Infrastructure;
 
 namespace Catalog.API
 {
@@ -25,6 +27,12 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<CatalogContext>(
+                    contextOptions => contextOptions.UseNpgsql(
+                        Configuration.GetConnectionString("ConnectionString")));
+
             services.AddControllers();
         }
 
